813b3cb65ad228c07083c715abb8f03296171009
gfes40504 <gfes40504@gmail.com>
2020-05-15 10:14:31 +0800

    群聚分析版

--- fhs.R
+++ fhs.R
@@ -80,30 +80,42 @@ pls_weighting = function(pls){
 Tsqr = function(dat,matrixcov){
   apply(dat, 1, function(i) { i %*% matrixcov %*% i })
 }
+
 df =read.csv("FHS_strk-4.csv")
 x_cols = c('AGE','SEX','SYSBP','DIABP','BPMEDS','CURSMOKE','CIGPDAY','TOTCHOL','HDLC','LDLC','BMI','GLUCOSE','HEARTRTE')
 x_df = df[,x_cols]
 x_df = knnImputation(x_df)
-y_cols = c('DIABETES','PREVAP','PREVCHD','PREVMI','PREVHYP')
+y_cols = c('DIABETES')
 y_df = df[,y_cols]
+n =length(x_cols)
+row = list()
+p_array=c()
+count = 1
+for(i in 1:(n-2)){
+  for(j in (i+1):(n-1)){
+    for(k in (j+1):n){
+      data = df[,x_cols[c(i,j,k)]]
+      na_df = is.na(data)
+      temp = (na_df[,1]+na_df[,2]+na_df[,3])>0
+      data=data[!temp,]
+      data=scale(data)
+      clus = kmeans(data,3)$cluster
+      anova_df = data.frame(x = clus,DIABETES = df$DIABETES[!temp])
+      p_value = anova(lm(DIABETES~x,data = anova_df))$`Pr(>F)`
+      row[[count]] = x_cols[c(i,j,k)]
+      p_array=c(p_array,p_value[1])   
+      count=count+1
+    }
+  }
+}
 
 
+result =table(unlist( row[which(p_array<0.05)]))
+result=sort(result,decreasing = T)
+result
 
-result = pls_part1(x_df,y_df)
-
-pls<-pls_part2(9,result$pls_overview)
-weight=pls_weighting(pls)
-xloading=pls$loadings[]
-yloading=pls$Yloadings[]
-key=rep(F,length(weight[,1]))
-
-
-
-weight_hyp=weight[order(weight[,"PREVHYP"],decreasing = T),]
-weight_dia=weight[order(weight[,"DIABETES"],decreasing = T),]
-
-#PREVHYP部分
-data_scale<-scale(x_df[,c('SYSBP','DIABP','AGE','BMI','GLUCOSE')])
+#A組
+data_scale<-scale(x_df[,names(result)[1:3]])
 corr<-cor(data_scale,use = "complete.obs")
 E<-eigen(corr)
 score<-data.frame(data_scale%*%E$vectors)
@@ -111,21 +123,19 @@ evs<-round(E$values,digits = 5)
 eigentable=data.frame("a"=evs,"b"=round(cumsum(evs)/sum(evs)*100,5))
 names(eigentable)=c("特徵值","累積解釋量(%)")
 row.names(eigentable)=paste("PC",1:length(eigentable[,1]),sep="")
-
 eigentable
 
-dat=scale(as.matrix(score[,1:3,drop=F]),T,F)
+#A組
+dat=scale(as.matrix(score[,1,drop=F]),T,F)
 varcov=cov(dat)
 matrixcov=ginv(varcov)
 tscore=Tsqr(dat = dat,matrixcov = matrixcov)
-
-
 mean_tscore = mean(tscore)
-anova_df_hyp = data.frame(range = as.numeric(tscore >mean_tscore)+1,PREVHYP = df$PREVHYP)
-anova(lm(PREVHYP~range,data = anova_df_hyp))
+anova_df_A = data.frame(range = as.numeric(tscore >mean_tscore)+1,DIABETES = df$DIABETES)
+anova(lm(DIABETES~range,data = anova_df_A))
 
-#DIABETES
-data_scale<-scale(x_df[,c('SYSBP','DIABP','GLUCOSE')])
+#B組
+data_scale<-scale(x_df[,names(result)[11:13]])
 corr<-cor(data_scale,use = "complete.obs")
 E<-eigen(corr)
 score<-data.frame(data_scale%*%E$vectors)
@@ -133,16 +143,13 @@ evs<-round(E$values,digits = 5)
 eigentable=data.frame("a"=evs,"b"=round(cumsum(evs)/sum(evs)*100,5))
 names(eigentable)=c("特徵值","累積解釋量(%)")
 row.names(eigentable)=paste("PC",1:length(eigentable[,1]),sep="")
-
 eigentable
-
+#B組
 dat=scale(as.matrix(score[,1:2,drop=F]),T,F)
 varcov=cov(dat)
 matrixcov=ginv(varcov)
 tscore=Tsqr(dat = dat,matrixcov = matrixcov)
-
-
 mean_tscore = mean(tscore)
-anova_df_dia = data.frame(range = as.numeric(tscore >mean_tscore)+1,DIABETES = df$DIABETES)
-anova(lm(DIABETES~range,data = anova_df_dia))
+anova_df_B = data.frame(range = as.numeric(tscore >mean_tscore)+1,DIABETES = df$DIABETES)
+anova(lm(DIABETES~range,data = anova_df_B))
 
